80c55218532fee5a3de53adc43430200b91bfad7
Thomas Endres <Thomas-Endres@gmx.de>
2011-02-11 10:29:56 +0100

    Updated legal information

--- ARDroneInput/JoystickInput.cs
+++ ARDroneInput/JoystickInput.cs
@@ -7,7 +7,6 @@
  * 
  * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
